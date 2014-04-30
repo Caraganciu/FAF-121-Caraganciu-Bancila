fabd4a7ec6850e493fb626a865424b905b02c36f
Bancila <Bancila@users.noreply.github.com>
2014-04-30 17:54:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,3 +32,27 @@ bool over = false, draw = false;
 short turnColor =1;
 short turn = 0, counter = 0;
 int bStatus[9] = {0};
+
+enum{
+    IDB_1, IDB_2, IDB_3, IDB_4, IDB_5, IDB_6, IDB_7, IDB_8, IDB_9,
+    IDS_TURN,
+    IDB_RESTART,
+    IDB_RESET,
+    IDB_SCORE,
+    IDS_TITLE,
+    IDS_XSCORE, IDS_OSCORE, IDS_DRAW_SCORE,
+    IDS_XVALUE, IDS_OVALUE, IDS_DRAW_VALUE,
+
+    };
+
+HINSTANCE hInstance;
+HWND hwnd1;
+HWND hwnd2;
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd);
+LRESULT CALLBACK MyWndProc1(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+//Windows Procedure for the game
+
+LRESULT CALLBACK MyWndProc2(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK AboutDlgProc(HWND, UINT, WPARAM, LPARAM);
